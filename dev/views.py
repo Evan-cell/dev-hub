3d005843da9ee9c5d96e69cd7dc474342aa20d89
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-10-03 14:14:54 +0300

    templates

--- dev/views.py
+++ dev/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def project(request,pk):
+    context = {}
+    return render(request, 'single-project.html' , context)
+
+def projects(request):
+    context = {}
+    return render(request, 'projects.html' , context)    
+
+    
